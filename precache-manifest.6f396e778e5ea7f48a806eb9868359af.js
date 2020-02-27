3b99d13534a5bfc4f2e009fae35faaf51e8795df
Alpesh Rameshbhai Thakkar <thakkaralpesh100@gmail.com>
2020-02-27 19:38:15 +0530

    Updates

---
+++ precache-manifest.6f396e778e5ea7f48a806eb9868359af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c006e020b1d287398783e08385056475",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4713da6fc08239ac2889",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8786d2ac7a76ec9071e8",
+    "url": "/static/js/2.b5838d42.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b5838d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4713da6fc08239ac2889",
+    "url": "/static/js/main.e2488a47.chunk.js"
+  },
+  {
+    "revision": "aa30810eb8a9a2803727",
+    "url": "/static/js/runtime-main.cc575097.js"
+  },
+  {
+    "revision": "8c636777842c1f8b146dc16b84c0b154",
+    "url": "/static/media/loading.8c636777.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
